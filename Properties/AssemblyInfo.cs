e9603e896cf5f682295ad3f889da88c373d77885
Dodjer <43703016+Dodjer@users.noreply.github.com>
2018-11-11 12:07:24 +0300

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+using ns0;
+
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyInfo("")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: Guid("c454d2d2-319e-43dd-8470-909894aaa1d6")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("Better Nike Bot")]
+[assembly: AssemblyProduct("Better Nike Bot")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
